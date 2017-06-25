717644a39ec6e5f3d4a804a57c4674389dc948e5
tushar bansal <tuskibansal@gmail.com>
2017-06-25 12:22:43 +0530

    few changes

--- application/views/signin.php
+++ application/views/signin.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8" />
   <title>SIGN IN</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="shortcut icon" type="image/x-icon" href="home.ico" />
+  <link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url();?>images/home.ico" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
