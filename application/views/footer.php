08e6cfc2781a92235ad82d998725aa0caca62fa7
vaibhavchellani <vaibhavchellani223@gmail.com>
2017-06-25 12:30:13 +0530

    no changes

---
+++ application/views/footer.php
@@ -0,0 +1,53 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+	<link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>css/footer.css">
+</head>
+<body>
+	
+
+	<footer class="footer-distributed">
+
+		<div class="footer-right">
+
+			<a href="https://www.facebook.com/tuskibansal"><i class="fa fa-facebook"></i></a>
+			<a href="https://twitter.com/login"><i class="fa fa-twitter"></i></a>
+			<a href="https://www.linkedin.com/mynetwork/"><i class="fa fa-linkedin"></i></a>
+			<a href="https://github.com/anonymous043044/startup-bvp1.1"><i class="fa fa-github"></i></a>
+
+		</div>
+
+		<div class="footer-left">
+
+			<p class="footer-links">
+				<a href="#">Home</a>
+				·
+				<a href="#about">About</a>
+				·
+				<a href="#contact">Contact</a>
+				·
+				<a href="signup/signin.php">Sign In</a>
+				·
+				<a href="php/signup.php">Sign Up</a>
+				
+			</p>
+
+			<p>Company Name &copy; 2017</p>
+		</div>
+
+	</footer>
+	<!-- footer end  here -->
+
+</body>
+</html>
