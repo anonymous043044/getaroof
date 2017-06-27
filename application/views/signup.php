b041d7d428d2d3dbb6c8c3ed2a05bfc624abdd63
tushar bansal <tuskibansal@gmail.com>
2017-06-28 04:37:37 +0530

    property_Details work done

--- application/views/signup.php
+++ application/views/signup.php
@@ -10,9 +10,12 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <style>
-    body , html{
+    body {
       overflow-x: hidden;
       
+    }
+    html{
+       overflow-x: hidden;
     }
     .navbar {
      margin-bottom: 0;
